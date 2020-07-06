3dc92c8c5475cf55e28a19f69096c04b2868f222
cyril <cyrilnee2513@gmail.com>
2020-07-07 01:08:11 +0800

    update wallpaperStyle change to FILL

--- wallpaper.py
+++ wallpaper.py
@@ -16,7 +16,7 @@ class Win32WallPaperSetter(IWallPaperSetter):
 
     def setWallpaper(self, imagepath):
         k = win32api.RegOpenKeyEx(win32con.HKEY_CURRENT_USER,"Control Panel\\Desktop",0,win32con.KEY_SET_VALUE)
-        win32api.RegSetValueEx(k, "WallpaperStyle", 0, win32con.REG_SZ, "0")
+        win32api.RegSetValueEx(k, "WallpaperStyle", 0, win32con.REG_SZ, "4")
         win32api.RegSetValueEx(k, "TileWallpaper", 0, win32con.REG_SZ, "0")
         win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER,imagepath, 1+2)
 
